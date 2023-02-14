83170766f9abd80bb26c7aed440896956e836b74
Revanth Madasu <revanthmadasu913@gmail.com>
2023-02-14 11:28:08 -0600

    feat: navbar moved to bottom in mobile view

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,33 +3,27 @@ import NavItem from './NavItem';
 import ExternalLinks from "./external-links";
 import React from 'react';
 import "./NavBar.scss";
+import { navItems } from 'data/navItems';
+import classNames from 'classnames';
+import { isMobile } from 'react-device-detect';
 export function NavBar() {
-    const navItems = [
+    const navItemTags = navItems.map(item => <NavItem navTitle={item.navTitle} navIcon={item.navIcon} navLink={item.navLink}></NavItem>);
+    const navClass = classNames(
+        "nav-container",
+        "text-center",
+        "position-fixed",
+        "w-100",
         {
-            navTitle: 'Home',
-            navIcon: 'fa-home',
-            navLink: '/home'
-        },
-        {
-            navTitle: 'Skills',
-            navIcon: 'fa-diamond',
-            navLink: '/skills'
-        },
-        {
-            navTitle: 'Experience',
-            navIcon: 'fa-briefcase',
-            navLink: '/experience'
-        },
-        {
-            navTitle: 'Projects',
-            navIcon: 'fa-file-code-o',
-            navLink: '/projects'
+            "nav-container-desktop": !isMobile,
+            "nav-container-mobile": isMobile,
+            "w3-animate-top": !isMobile,
+            "pt-3": !isMobile,
+            "w3-animate-bottom": isMobile,
         }
-    ];
-    const navItemTags = navItems.map(item => <NavItem navTitle={item.navTitle} navIcon={item.navIcon} navLink={item.navLink}></NavItem>)
+    );
     return (
         <React.Fragment>
-            <div className="nav-container text-center position-fixed w-100 w3-animate-top pt-3">
+            <div className={navClass}>
                 {navItemTags}
                 <span className="d-inline-block float-right mr-lg-5 mr-md-4 mr-sm-2">
                     <ExternalLinks></ExternalLinks>
---
+++ src/data/navItems.js
@@ -0,0 +1,22 @@
+export const navItems = [
+    {
+        navTitle: 'Home',
+        navIcon: 'fa-home',
+        navLink: '/home'
+    },
+    {
+        navTitle: 'Skills',
+        navIcon: 'fa-diamond',
+        navLink: '/skills'
+    },
+    {
+        navTitle: 'Experience',
+        navIcon: 'fa-briefcase',
+        navLink: '/experience'
+    },
+    {
+        navTitle: 'Projects',
+        navIcon: 'fa-file-code-o',
+        navLink: '/projects'
+    }
+];
